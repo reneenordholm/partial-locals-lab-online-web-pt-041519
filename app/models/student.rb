88fbac0b372371698d52ba5ddf5dd8ec1e067559
Renee Nordholm <reneenordholm@gmail.com>
2019-09-16 15:23:21 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  # Add in search functionality such that users can search 
+  # for a student by name and see all matching results on the 
+  # students index page. The results should be displayed by 
+  # rendering a students/_student.html.erb partial.
+  def self.search(name)
+    Student.where("name LIKE ?", "%#{name}%")
+  end
 end
